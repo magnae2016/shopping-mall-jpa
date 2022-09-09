ffd9269c0692fa6ec2178e98fbbad3671e8c9b49
magnae2016 <magnae2016@gmail.com>
2022-09-09 22:02:53 +0900

    Add #13 - lazy loading and test

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -26,7 +26,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
@@ -36,7 +36,8 @@ public class Order {
     private OrderStatus orderStatus;
 
     @Builder.Default
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval =
+            true, fetch = FetchType.LAZY)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @CreatedDate
--- src/test/java/com/shop/entity/OrderTest.java
+++ src/test/java/com/shop/entity/OrderTest.java
@@ -4,6 +4,7 @@ import com.shop.constant.ItemSellStatus;
 import com.shop.constant.Role;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
+import com.shop.repository.OrderItemRepository;
 import com.shop.repository.OrderRepository;
 import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.DisplayName;
@@ -35,6 +36,9 @@ class OrderTest {
 
     @Autowired
     MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
 
     public Item createItem() {
         return Item.builder()
@@ -108,5 +112,21 @@ class OrderTest {
         Order order = createOrder();
         order.getOrderItems().remove(0);
         em.flush();
+    }
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    public void lazyLoadingTest() {
+        Order order = createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem =
+                orderItemRepository.findById(orderItemId).orElseThrow(EntityNotFoundException::new);
+        System.out.println("Order class : " + orderItem.getOrder().getClass());
+        System.out.println("===========================");
+        orderItem.getOrder().getOrderDate();
+        System.out.println("===========================");
     }
 }
