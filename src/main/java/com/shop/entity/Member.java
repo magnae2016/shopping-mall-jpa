814b6621e9d79e574ea4d1cd63626f68a49f4a83
magnae2016 <magnae2016@gmail.com>
2022-09-09 11:50:16 +0900

    Add #8 - Member entity
    Member 엔티티에 회원을 생성하는 메소드를 만들어서 관리를 한다면 코드가 변경되더라도 한 군데만 수정하면 되는 이점이 있습니다.

---
+++ src/main/java/com/shop/entity/Member.java
@@ -0,0 +1,48 @@
+package com.shop.entity;
+
+import com.shop.constant.Role;
+import com.shop.dto.MemberFromDto;
+import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "member")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private String address;
+
+    private Role role;
+
+    public static Member createMember(MemberFromDto memberFromDto,
+                                      PasswordEncoder passwordEncoder) {
+        String password = passwordEncoder.encode(memberFromDto.getPassword());
+        Member member = Member.builder()
+                .name(memberFromDto.getName())
+                .email(memberFromDto.getEmail())
+                .address(memberFromDto.getAddress())
+                .password(password)
+                .role(Role.USER)
+                .build();
+        return member;
+    }
+}
