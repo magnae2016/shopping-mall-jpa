682c1bc8d07fbd3024b8a57b7a5586150a68a617
magnae2016 <magnae2016@gmail.com>
2022-09-09 13:08:14 +0900

    Fix #8 - problem for the constructor generated by @Builder is package private
    https://stackoverflow.com/a/71414919

--- src/main/java/com/shop/dto/MemberFromDto.java
+++ src/main/java/com/shop/dto/MemberFromDto.java
@@ -1,12 +1,12 @@
 package com.shop.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberFromDto {
 
     private String name;
