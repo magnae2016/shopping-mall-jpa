6dd6b0db4480e787002c3683d358997681ef4cbd
magnae2016 <magnae2016@gmail.com>
2022-09-09 20:49:30 +0900

    Add #11 - cart_item for ManyToOne

---
+++ src/main/java/com/shop/entity/CartItem.java
@@ -0,0 +1,30 @@
+package com.shop.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart_item")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    private int count;
+}
